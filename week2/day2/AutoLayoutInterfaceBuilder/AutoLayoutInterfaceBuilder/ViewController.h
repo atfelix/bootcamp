f1dea69fe112f8611b040a5e517e002a26d4d0cb
Adam Felix <adam.tyler.felix@gmail.com>
2017-05-09 16:16:47 -0400

    Add Auto Layout Interface Builder

---
+++ week2/day2/AutoLayoutInterfaceBuilder/AutoLayoutInterfaceBuilder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayoutInterfaceBuilder
+//
+//  Created by atfelix on 2017-05-09.
+//  Copyright © 2017 Adam Felix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
