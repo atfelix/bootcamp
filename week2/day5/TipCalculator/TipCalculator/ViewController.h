647a789ded54ddd19218cac6c2d295649d1df59b
Adam Felix <adam.tyler.felix@gmail.com>
2017-05-15 19:41:30 -0400

    Initial commit for Tip Calculator

---
+++ week2/day5/TipCalculator/TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by atfelix on 2017-05-15.
+//  Copyright © 2017 Adam Felix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
