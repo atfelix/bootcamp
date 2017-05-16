1e6234c9afbb254604337b4fa5ca8f2151db4c44
Adam Felix <adam.tyler.felix@gmail.com>
2017-05-15 20:48:26 -0400

    Finish Stretch Goal 1 and dismiss keyboard functionality

--- week2/day5/TipCalculator/TipCalculator/ViewController.m
+++ week2/day5/TipCalculator/TipCalculator/ViewController.m
@@ -13,6 +13,7 @@
 @interface ViewController ()
 
 @property (weak, nonatomic) IBOutlet UITextField *billAmountTextField;
+@property (weak, nonatomic) IBOutlet UITextField *tipPercentageField;
 @property (weak, nonatomic) IBOutlet UILabel *tipAmountLabel;
 
 @end
@@ -21,7 +22,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.tipAmountLabel.text = @"";
+
+    self.tipAmountLabel.text = @"$";
+    [self registerKeyboardNotifications];
 }
 
 
@@ -31,7 +34,59 @@
 }
 
 - (IBAction)calculateTip:(UIButton *)sender {
-    self.tipAmountLabel.text = [NSString stringWithFormat:@"$%.2f", TipRate * [self.billAmountTextField.text doubleValue]];
+    self.tipAmountLabel.text = [NSString stringWithFormat:@"$%.2f", [self.tipPercentageField.text doubleValue] * [self.billAmountTextField.text doubleValue] / 100.0];
+    [self.view endEditing:YES];
 }
+
+-(void)registerKeyboardNotifications {
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardShowsOnScreen:)
+                                                 name:UIKeyboardDidShowNotification
+                                               object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardHidesOffScreen:)
+                                                 name:UIKeyboardDidHideNotification
+                                               object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardDidShowOnScreen:)
+                                                 name:UIKeyboardDidShowNotification
+                                               object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardDidHideOffScreen:)
+                                                 name:UIKeyboardDidHideNotification
+                                               object:nil];
+}
+
+-(void)keyboardShowsOnScreen:(NSNotification *)notification {
+    CGRect rect = [notification.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue];
+
+    if (CGRectContainsPoint(rect, self.billAmountTextField.frame.origin)) {
+        self.billAmountTextField.frame = CGRectMake(self.billAmountTextField.frame.origin.x,
+                                                    self.billAmountTextField.frame.origin.y - rect.size.height,
+                                                    self.billAmountTextField.frame.size.width,
+                                                    self.billAmountTextField.frame.size.height);
+    }
+
+    if (CGRectContainsPoint(rect, self.tipPercentageField.frame.origin)) {
+        self.tipPercentageField.frame = CGRectMake(self.tipPercentageField.frame.origin.x,
+                                                    self.tipPercentageField.frame.origin.y - rect.size.height,
+                                                    self.tipPercentageField.frame.size.width,
+                                                    self.tipPercentageField.frame.size.height);
+    }
+}
+
+-(void)keyboardHidesOffScreen:(NSNotification *)notification {
+}
+
+-(void)keyboardDidShowOnScreen:(NSNotification *)notification {
+}
+
+-(void)keyboardDidHideOffScreen:(NSNotification *)notification {
+}
+
+- (IBAction)dismissKeyboard:(UITapGestureRecognizer *)sender {
+    [self.view endEditing:YES];
+}
+
 
 @end
