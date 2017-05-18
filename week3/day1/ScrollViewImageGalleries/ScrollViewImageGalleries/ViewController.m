5f30db84a45fc531387defba4e79209ad180e50e
Adam Felix <adam.tyler.felix@gmail.com>
2017-05-17 20:30:58 -0400

    Finish Part 1

--- week3/day1/ScrollViewImageGalleries/ScrollViewImageGalleries/ViewController.m
+++ week3/day1/ScrollViewImageGalleries/ScrollViewImageGalleries/ViewController.m
@@ -8,7 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIScrollViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (nonatomic) UIPageControl *control;
+@property (nonatomic) NSMutableArray<UIImageView *> *imageViewArray;
 
 @end
 
@@ -16,7 +20,15 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    self.imageViewArray = [[NSMutableArray alloc] init];
+
+    [self createImageViews];
+
+    self.scrollView.pagingEnabled = YES;
+    self.scrollView.delegate = self;
+
+    [self addConstraintToImageViews];
 }
 
 
@@ -25,5 +37,55 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)createImageViews {
+
+    for (NSString *name in @[@"Lighthouse-in-Field.jpg", @"Lighthouse-night.jpg", @"Lighthouse.jpg"]) {
+
+        UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:name]];
+
+        imageView.contentMode = UIViewContentModeScaleAspectFit;
+        imageView.clipsToBounds = YES;
+
+        [self.imageViewArray addObject:imageView];
+    }
+
+    for (UIImageView* imageView in self.imageViewArray) {
+        [self.scrollView addSubview:imageView];
+    }
+
+}
+
+-(void)addConstraintToImageViews {
+
+    [self translatesAutoresizingMasksToConstraints];
+
+    CGFloat width = self.view.bounds.size.width;
+
+    self.scrollView.contentSize = CGSizeMake(width * self.imageViewArray.count,
+                                             self.view.frame.size.height);
+
+    for (int i = 0; i < self.imageViewArray.count; i++) {
+        UIImageView *imageView = self.imageViewArray[i];
+
+        [imageView.topAnchor constraintEqualToAnchor:self.view.topAnchor].active = YES;
+        [imageView.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor].active = YES;
+        [imageView.widthAnchor constraintEqualToConstant:width].active = YES;
+
+        if (i == 0) {
+            [imageView.leadingAnchor constraintEqualToAnchor:self.scrollView.leadingAnchor].active = YES;
+        }
+        else {
+            [imageView.leadingAnchor constraintEqualToAnchor:self.imageViewArray[i - 1].trailingAnchor].active = YES;
+        }
+    }
+    [self.imageViewArray[self.imageViewArray.count - 1].trailingAnchor constraintEqualToAnchor:self.scrollView.trailingAnchor ].active = YES;
+
+}
+
+-(void)translatesAutoresizingMasksToConstraints {
+    for (UIImageView *iv in self.imageViewArray) {
+        iv.translatesAutoresizingMaskIntoConstraints = NO;
+    }
+}
 
 @end
