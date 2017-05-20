0c0508b734a38029c11a3f9155be5a0db149ddf3
Adam Felix <adam.tyler.felix@gmail.com>
2017-05-20 08:03:18 -0400

    initial

---
+++ week2/day5/FingerPaint/FingerPaint/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FingerPaint
+//
+//  Created by atfelix on 2017-05-20.
+//  Copyright © 2017 Adam Felix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ week2/day5/FingerPaint/FingerPaint/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FingerPaint
+//
+//  Created by atfelix on 2017-05-20.
+//  Copyright © 2017 Adam Felix. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
