db8568a5abc87c8b0200547e24111f258cd57361
Adam Felix <adam.tyler.felix@gmail.com>
2017-05-24 19:27:46 -0400

    Initial Commit for Every Do Core Data

---
+++ week4/day3/EveryDo/EveryDo/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  EveryDo
+//
+//  Created by atfelix on 2017-05-24.
+//  Copyright © 2017 Adam Felix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
