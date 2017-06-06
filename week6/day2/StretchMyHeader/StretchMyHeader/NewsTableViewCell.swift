ae95acca9429ef9a30685ab839f2244734dae878
Adam Felix <adam.tyler.felix@gmail.com>
2017-06-06 10:27:42 -0400

    Initial commit for Stretch My Header assignment

---
+++ week6/day2/StretchMyHeader/StretchMyHeader/NewsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewsTableViewCell.swift
+//  StretchMyHeader
+//
+//  Created by atfelix on 2017-06-06.
+//  Copyright © 2017 Adam Felix. All rights reserved.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
