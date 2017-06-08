4da71a05ace40128ce9fcc2ffbd2fb293976d281
Adam Felix <adam.tyler.felix@gmail.com>
2017-06-08 10:35:06 -0400

    Add plus button and stub action

--- week6/day4/StretchySnacks/StretchySnacks/ViewController.swift
+++ week6/day4/StretchySnacks/StretchySnacks/ViewController.swift
@@ -20,6 +20,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func plusButtonTapped(_ sender: UIButton) {
+        print("plus button tapped")
+    }
 }
-
