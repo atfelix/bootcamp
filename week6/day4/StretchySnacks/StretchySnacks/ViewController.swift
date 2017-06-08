fd5e7d4c70dda8b1354be3d9147a84597ebd53d7
Adam Felix <adam.tyler.felix@gmail.com>
2017-06-08 15:08:15 -0400

    Finish assignment Stretchy snacks

--- week6/day4/StretchySnacks/StretchySnacks/ViewController.swift
+++ week6/day4/StretchySnacks/StretchySnacks/ViewController.swift
@@ -8,12 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
     @IBOutlet weak var stretchyHeaderHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var stretchyNavigationBarView: UIView!
+    @IBOutlet weak var tableView: UITableView!
+
+    var snacks = [String]()
     var stackView : UIStackView!
     var isStretched = false
+
     private var stackViewOriginalFrame: CGRect!
     private var stackViewStretchedFrame: CGRect!
 
@@ -28,14 +32,30 @@ class ViewController: UIViewController {
         isStretched = !isStretched
     }
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return snacks.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SnackCell", for: indexPath)
+        cell.textLabel?.text = snacks[indexPath.row]
+
+        return cell
+    }
+
     private func addStackView() {
         let imageNames = ["oreos.png", "pizza_pockets.png", "pop_tarts.png", "popsicle.png", "ramen.png"]
         stackViewOriginalFrame = CGRect(x: 0, y: 50, width: view.bounds.width, height: 0)
         stackViewStretchedFrame = CGRect(x: 0, y: 70, width: view.bounds.width, height: 120)
         stackView = UIStackView(frame: stackViewOriginalFrame)
         stackView.distribution = .fillEqually
+        stackView.isUserInteractionEnabled = true
         for i in 0..<5 {
-            stackView.addArrangedSubview(UIImageView(image: UIImage(named: imageNames[i])))
+            stackView.addArrangedSubview(imageViewWithTapGR(imageName: imageNames[i]))
         }
         stretchyNavigationBarView.addSubview(stackView)
     }
@@ -51,7 +71,6 @@ class ViewController: UIViewController {
                        initialSpringVelocity: 0.7,
                        options: [.curveEaseInOut],
                        animations: {
-
                            self.stackView.frame = (self.isStretched) ? self.stackViewOriginalFrame : self.stackViewStretchedFrame
                            sender.transform = sender.transform.rotated(by: CGFloat.pi / 4)
                            self.view.layoutIfNeeded()
@@ -63,10 +82,36 @@ class ViewController: UIViewController {
         for view in view.subviews {
             for constraint in view.constraints {
                 if constraint.identifier == "centerYSnackTitle" {
+                    let label = constraint.firstItem as! UILabel
+                    label.text = (isStretched) ? "SNACKS" : "Add a Snack"
                     constraint.isActive = false
                     constraint.constant -= (isStretched) ? -40 : 40
                     constraint.isActive = true
                 }
+            }
+        }
+    }
+
+    private func imageViewWithTapGR(imageName: String) -> UIImageView {
+        let imageView = UIImageView(image: UIImage(named: imageName))
+        imageView.isUserInteractionEnabled = true
+        addTapGestureRecognizer(to: imageView)
+        return imageView
+    }
+
+    private func addTapGestureRecognizer(to imageView: UIImageView) {
+        let tapGR = UITapGestureRecognizer(target: self, action: #selector(ViewController.addSnack(_:)))
+        imageView.addGestureRecognizer(tapGR)
+    }
+
+    func addSnack(_ sender: UITapGestureRecognizer) {
+        let names = ["Oreos", "Pizza Pockets", "Pop Tarts", "Popsicle", "Ramen"]
+        let imageView = sender.view as! UIImageView
+        for (i, view) in stackView.arrangedSubviews.enumerated() {
+            if view === imageView {
+                snacks.append(names[i])
+                tableView.reloadData()
+                return
             }
         }
     }
