7ea891237fecbed8a73eb86ca3355e11b95ebc80
Adam Felix <adam.tyler.felix@gmail.com>
2017-06-08 11:42:41 -0400

    add animation to button and toggle feature by clicking on same button

--- week6/day4/StretchySnacks/StretchySnacks/ViewController.swift
+++ week6/day4/StretchySnacks/StretchySnacks/ViewController.swift
@@ -11,10 +11,12 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var stretchyHeaderHeightConstraint: NSLayoutConstraint!
+    var isStretched = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print(stretchyHeaderHeightConstraint.constant)
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,10 +25,23 @@ class ViewController: UIViewController {
     }
 
     @IBAction func plusButtonTapped(_ sender: UIButton) {
-        self.stretchyHeaderHeightConstraint.constant = 200
+        animateConstraints(sender, newConstant: (isStretched) ? 64 : 200)
+        isStretched = !isStretched
+    }
+
+    private func animateConstraints(_ sender: UIButton, newConstant: CGFloat) {
+        self.stretchyHeaderHeightConstraint.constant = newConstant
         view.setNeedsUpdateConstraints()
-        UIView.animate(withDuration: 1.0) {
-            self.view.layoutIfNeeded()
-        }
+
+        UIView.animate(withDuration: 1.3,
+                       delay: 0.0,
+                       usingSpringWithDamping: 0.33,
+                       initialSpringVelocity: 0.7,
+                       options: [.curveEaseInOut],
+                       animations: {
+                           sender.transform = sender.transform.rotated(by: CGFloat.pi / 4)
+                           self.view.layoutIfNeeded()
+        },
+                       completion: nil)
     }
 }
